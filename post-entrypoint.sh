105207e3653b7835345826ef913866e225bb503d
Matt Vague <mattvague@gmail.com>
2023-01-10 12:20:01 -0800

    Fix env var name

--- post-entrypoint.sh
+++ post-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $CLEANUP_THEME = 'true' ]]; then
+if [[ $INPUT_CLEANUP_THEME = 'true' ]]; then
   echo "Cleaning development theme..."
   shopify shopify theme delete -f -d
 fi
