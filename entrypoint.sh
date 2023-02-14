70cabf3d1e658b8cdcfe215034f7c66ecb3e2426
Matt Vague <mattvague@gmail.com>
2023-02-14 09:14:15 -0800

    Upgrade to shopify CLI v3

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,7 +80,7 @@ theme_root="${THEME_ROOT:-.}"
 
 step "Creating development theme"
 theme_push_log="$(mktemp)"
-shopify theme push --development --json $theme_root > "$theme_push_log" && cat "$theme_push_log"
+shopify theme push --development --path=$theme_root > "$theme_push_log" && cat "$theme_push_log"
 preview_url="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.preview_url')"
 editor_url="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.editor_url')"
 preview_id="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.id')"
