495278a22056eacc838769b947f4bf910134472f
Matt Vague <mattvague@gmail.com>
2023-02-15 17:40:50 -0800

    Fix url extraction

--- entrypoint.sh
+++ entrypoint.sh
@@ -91,10 +91,12 @@ fi
 
 echo $theme_push_log
 
-preview_url="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.preview_url')"
-editor_url="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.editor_url')"
-preview_id="$(cat "$theme_push_log" | tail -n 1 | jq -r '.theme.id')"
+preview_url="$(cat "$theme_push_log" | awk '/View your theme:/ { getline; print }'"
+editor_url="$(cat "$theme_push_log" | awk '/Customize this theme in the Theme Editor:/ { getline; print }'"
+
+# TODO
+# preview_id=".."
 
 echo "preview_url=$preview_url" >> $GITHUB_OUTPUT
 echo "editor_url=$editor_url" >> $GITHUB_OUTPUT
-echo "theme_id=$preview_id" >> $GITHUB_OUTPUT
+# echo "theme_id=$preview_id" >> $GITHUB_OUTPUT
